99c00443beeee6fd285ff36b24b3e8392a404e0c
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-01 20:45:34 +0600

    layout, header, footer and hero section design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Layout from "./layout/Layout"
 const App = () => {
-    return <div>App</div>
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Layout>
+                            <p>Home Page</p>
+                        </Layout>
+                    }
+                />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
