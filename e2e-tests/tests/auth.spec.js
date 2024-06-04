021387cd2d14a5100352c850a094a1673fe99677
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-04 14:41:07 +0600

    sign in and sign out e2e testing

---
+++ e2e-tests/tests/auth.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+
+const FRONTEND_URL = "http://localhost:5173/";
+
+// testing user login
+test("should allow the user to sign in", async ({ page }) => {
+    await page.goto(FRONTEND_URL);
+
+    await page.getByRole("link", { name: "Sign In" }).click();
+
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+    await page.locator("[name=email]").fill("test@gmail.com");
+    await page.locator("[name=password]").fill("test");
+
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    await expect(page.getByText("welcome back, test man")).toBeVisible();
+    await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+
+// testing user registration
+test("should allow user to register", async ({ page }) => {
+    await page.goto(FRONTEND_URL);
+
+    await page.getByRole("link", { name: "Sign In" }).click();
+    await page.getByRole("link", { name: "Sign Up" }).click();
+
+    await expect(
+        page.getByRole("heading", { name: "Create an Account" })
+    ).toBeVisible();
+
+    await page.locator("[name=firstName]").fill("test");
+    await page.locator("[name=lastName]").fill("man");
+    await page.locator("[name=email]").fill("test@gmail.com");
+    await page.locator("[name=password]").fill("test");
+    await page.locator("[name=confirmPassword]").fill("test");
+
+    await page.getByRole("button", { name: "Create Account" }).click();
+
+    await expect(page.getByText("user registered successfully")).toBeVisible();
+});
--- server/src/secret.js
+++ server/src/secret.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const port = process.env.SERVER_PORT || 5000
-const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/MERN"
+const mongoURL = process.env.MONGO_URL
 const jwtAccessKey = process.env.JWT_ACESS_KEY
 
 
