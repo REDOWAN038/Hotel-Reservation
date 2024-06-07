75453143f15926ed9392aa52cfcc83cb2aa244c9
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-07 21:40:47 +0600

    view hotel testing

--- e2e-tests/tests/hotel.spec.js
+++ e2e-tests/tests/hotel.spec.js
@@ -47,3 +47,20 @@ test("should allow user to add a hotel", async ({ page }) => {
     await page.getByRole("button", { name: "Save" }).click();
     await expect(page.getByText("hotel added successfully")).toBeVisible();
 });
+
+test("should display hotels", async ({ page }) => {
+    await page.goto(`${FRONTEND_URL}my-hotels`);
+
+    await expect(page.getByRole("heading", { name: "Test Hotel" })).toBeVisible();
+    await expect(page.getByText("This is a description for the Test Hotel")).toBeVisible();
+    await expect(page.getByText("Test City, Test Country")).toBeVisible();
+    await expect(page.getByText("Budget")).toBeVisible();
+    await expect(page.getByText("100 per night")).toBeVisible();
+    await expect(page.getByText("2 adults, 4 children")).toBeVisible();
+    await expect(page.getByText("3 Star Rating")).toBeVisible();
+
+    await expect(
+        page.getByRole("link", { name: "View Details" }).first()
+    ).toBeVisible();
+    await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});
