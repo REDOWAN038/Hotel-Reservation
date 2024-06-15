87107dfdcb478c941ac092df83bbbe2fe4dc2e46
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-06-15 15:18:24 +0600

    responsive navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { selectIsLoggedIn } from "../features/auth/selector"
+import { FiMenu } from "react-icons/fi"
+import HeaderModal from "../modal/HeaderModal"
 import SignOutButton from "./SignOutButton"
 
 const Header = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn)
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleModal = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div className='bg-[#003580] py-6'>
             <div className='container mx-auto flex justify-between'>
                 <span className='text-xl md:text-2xl text-white font-bold tracking-tight'>
                     <Link to='/'>CholoGhurteJai</Link>
                 </span>
-                <span className='flex gap-0 md:space-x-2'>
+                <span className='hidden sm:flex gap-0 md:space-x-2'>
                     {isLoggedIn ? (
                         <>
                             <Link
@@ -37,7 +46,26 @@ const Header = () => {
                         </Link>
                     )}
                 </span>
+                <span className='sm:hidden flex items-center'>
+                    {isLoggedIn ? (
+                        <button onClick={toggleModal} className='text-white'>
+                            <FiMenu className='h-6 w-6' />
+                        </button>
+                    ) : (
+                        <Link
+                            to='/signin'
+                            className='flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100 rounded-md'
+                        >
+                            Sign In
+                        </Link>
+                    )}
+                </span>
             </div>
+            <HeaderModal
+                isOpen={isOpen}
+                toggleModal={toggleModal}
+                isLoggedIn={isLoggedIn}
+            />
         </div>
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {},
     container: {
       padding: {
+        DEFAULT: "2rem",
         md: "10rem",
       },
     },
